c70d7a577058baea9b04ac97fe6e89d7efd5c4d1
Rilsikane <jutha.se@gmail.com>
2017-09-01 23:41:33 +0700

    for upload file

--- hkt-room-attach/src/main/java/com/hkt/roomAttach/controller/RoomAttachController.java
+++ hkt-room-attach/src/main/java/com/hkt/roomAttach/controller/RoomAttachController.java
@@ -8,11 +8,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.hkt.roomAttach.dto.AttachDto;
 import com.hkt.roomAttach.service.RoomAttachService;
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import com.jcraft.jsch.SftpException;
+
+
 
 @RestController
 public class RoomAttachController {
@@ -24,5 +34,32 @@ public class RoomAttachController {
 	@RequestMapping(value = "/searchRoomAttach.service", method = { RequestMethod.POST })
 	public List<AttachDto> searchRoomAttach(@RequestHeader HttpHeaders headers, @RequestBody AttachDto criteria){
 		return roomAttachService.searchRoomAttach(criteria);
+	}
+	@ResponseBody
+	@RequestMapping(value = "/uploadFile.service", method = { RequestMethod.POST })
+	public String uploadfile(@RequestHeader HttpHeaders headers, @RequestBody AttachDto criteria,@RequestParam MultipartFile file){
+		 JSch jsch = new JSch();
+	        Session session = null;
+	        try {
+	            session = jsch.getSession("root", "128.199.125.246", 22);
+	            session.setConfig("StrictHostKeyChecking", "no");
+	            session.setPassword("oms123456");
+	            session.connect();
+	            
+	            Channel channel = session.openChannel("sftp");
+	            channel.connect();
+	            ChannelSftp sftpChannel = (ChannelSftp) channel;
+	            sftpChannel.put(file.getInputStream(), "/home/testUpload.jpg");
+	            sftpChannel.exit();
+	            session.disconnect();
+	        } catch (JSchException e) {
+	            e.printStackTrace();  
+	        } catch (SftpException e) {
+	            e.printStackTrace();
+	        }catch (Exception e) {
+	            e.printStackTrace();
+	        }
+		
+		return "Upload Complete";
 	}
 }
