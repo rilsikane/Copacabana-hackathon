ae273e7de790e5c554be5073ae6b6ae8ca0d322e
Piyapat Maneetase <c02208@g-able.com>
2017-09-02 11:10:42 +0700

    no message

--- hkt-user/src/main/java/com/hkt/user/dao/UserDao.java
+++ hkt-user/src/main/java/com/hkt/user/dao/UserDao.java
@@ -38,18 +38,19 @@ public class UserDao extends JdbcDaoSupport {
 			}
 
 			String sql2 = " INSERT INTO user(username, password, first_name, last_name, gender) VALUES (?, ?, ?, ? ,?) ";
-			getJdbcTemplate().update(sql2, user.getUserName(), user.getPassword(), user.getFirstName(), user.getLastName(), user.getGender());
-			
+			getJdbcTemplate().update(sql2, user.getUserName(), user.getPassword(), user.getFirstName(),
+					user.getLastName(), user.getGender());
+
 			sql = new StringBuffer();
 			sql.append(" select * from user ");
 			sql.append(" where username = " + user.getUserName());
 			rows = getJdbcTemplate().queryForList(sql.toString());
 			if (!rows.isEmpty()) {
 				Map row = rows.get(0);
-				user.setUserNo((String)row.get("userNo"));
+				user.setUserNo((String) row.get("userNo"));
 				user.setMsg("101");
 			}
-			
+
 			user.setMsg("100");
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -58,69 +59,98 @@ public class UserDao extends JdbcDaoSupport {
 		}
 		return user;
 	}
-		
-	public UserDto login(UserDto user){
-		
-		try{
+
+	public UserDto login(UserDto user) {
+
+		try {
 			StringBuffer sql = new StringBuffer();
-			
-			if(user.getFacebookToken() != null){
+
+			if (user.getFacebookToken() != null) {
 				sql.append(" select * from user_info ");
 				sql.append(" where facebook_token = '" + user.getFacebookToken() + "'");
 				List<Map<String, Object>> rows = getJdbcTemplate().queryForList(sql.toString());
 				if (!rows.isEmpty()) {
 					UserDto result = new UserDto();
 					Map row = rows.get(0);
-					
-					result.setUserName((String)row.get("username"));
-					result.setPassword((String)row.get("password"));
-					result.setFirstName((String)row.get("first_name"));
-					result.setLastName((String)row.get("last_name"));
-					result.setGender((String)row.get("gender"));
-					result.setAddress((String)row.get("address"));
-					result.setTel((String)row.get("tel"));
-					result.setRole((String)row.get("role"));
-					result.setUserNo((String)row.get("userNo"));
-					
-					if(!result.getPassword().equals(user.getPassword())){
-						user.setMsg("104");//wrong token
+
+					result.setUserName((String) row.get("username"));
+					result.setPassword((String) row.get("password"));
+					result.setFirstName((String) row.get("first_name"));
+					result.setLastName((String) row.get("last_name"));
+					result.setGender((String) row.get("gender"));
+					result.setAddress((String) row.get("address"));
+					result.setTel((String) row.get("tel"));
+					result.setRole((String) row.get("role"));
+					result.setUserNo((String) row.get("userNo"));
+
+					if (!result.getPassword().equals(user.getPassword())) {
+						user.setMsg("104");// wrong token
 						return user;
-					}else{
+					} else {
 						result.setMsg("100");
 						return result;
 					}
-				}else{
+				} else {
+					user.setMsg("104");// wrong token
+					return user;
+				}
+			} else if (user.getGoogleToken() != null) {
+				sql.append(" select * from user_info ");
+				sql.append(" where google_token = '" + user.getGoogleToken() + "'");
+				List<Map<String, Object>> rows = getJdbcTemplate().queryForList(sql.toString());
+				if (!rows.isEmpty()) {
+					UserDto result = new UserDto();
+					Map row = rows.get(0);
+
+					result.setUserName((String) row.get("username"));
+					result.setPassword((String) row.get("password"));
+					result.setFirstName((String) row.get("first_name"));
+					result.setLastName((String) row.get("last_name"));
+					result.setGender((String) row.get("gender"));
+					result.setAddress((String) row.get("address"));
+					result.setTel((String) row.get("tel"));
+					result.setRole((String) row.get("role"));
+					result.setUserNo((String) row.get("userNo"));
+
+					if (!result.getPassword().equals(user.getPassword())) {
+						user.setMsg("104");// wrong token
+						return user;
+					} else {
+						result.setMsg("100");
+						return result;
+					}
+				} else {
+					user.setMsg("104");// wrong token
+					return user;
+				}
+			} else {
+				sql.append(" select * from user_info ");
+				sql.append(" where username = '" + user.getUserName() + "'");
+
+				List<Map<String, Object>> rows = getJdbcTemplate().queryForList(sql.toString());
+				if (!rows.isEmpty()) {
+					UserDto result = new UserDto();
+					Map row = rows.get(0);
+
+					result.setUserName((String) row.get("username"));
+					result.setPassword((String) row.get("password"));
+					result.setFirstName((String) row.get("first_name"));
+					result.setLastName((String) row.get("last_name"));
+					result.setGender((String) row.get("gender"));
+
+					if (!result.getPassword().equals(user.getPassword())) {
+						user.setMsg("103");// wrong password
+						return user;
+					} else {
+						result.setMsg("100");
+						return result;
+					}
+				} else {
 					user.setMsg("102");// wrong username
 					return user;
 				}
 			}
-
-			sql.append(" select * from user_info ");
-			sql.append(" where username = '" + user.getUserName() + "'");
-
-			List<Map<String, Object>> rows = getJdbcTemplate().queryForList(sql.toString());
-			if (!rows.isEmpty()) {
-				UserDto result = new UserDto();
-				Map row = rows.get(0);
-				
-				result.setUserName((String)row.get("username"));
-				result.setPassword((String)row.get("password"));
-				result.setFirstName((String)row.get("first_name"));
-				result.setLastName((String)row.get("last_name"));
-				result.setGender((String)row.get("gender"));
-				
-				if(!result.getPassword().equals(user.getPassword())){
-					user.setMsg("103");//wrong password
-					return user;
-				}else{
-					result.setMsg("100");
-					return result;
-				}
-			}else{
-				user.setMsg("102");// wrong username
-				return user;
-			}
-		}catch (Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println(e.getMessage());
 			user.setMsg("200");
